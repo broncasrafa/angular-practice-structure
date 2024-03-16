15cf08588c0ff97dcaf697f7c1c63f9b728d19a9
Rafael Francisco <broncasrafa@gmail.com>
2024-03-16 12:29:19 -0300

    👉🏾 adding core components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
